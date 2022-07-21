13043d893cbbd734968fc5cb7120c037432a2604
Gopesh <gopeshtiwari2009@gmail.com>
2022-07-21 22:19:54 +0530

    fetch implemented for users

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+export interface Monster {
+  id: string;
+  name: string;
+}
+
 function App() {
+ 
+  fetch('https://jsonplaceholder.typicode.com/users')
+  .then(response => response.json())
+  .then(users => {
+    setIsLoading(false);
+    setMonsters(users);
+  })
+  const [isLoading , setIsLoading] = useState(true);
+  const initialMonsters: Monster[] = [];
+  const [monsters, setMonsters] = useState(initialMonsters);
+  const [searchKey, setSearchKey] = useState('');
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p><h1>Atithi Devi Bhav</h1></p>
-      </header>
-      <p> <h3> Welcome to our Page! The main features will be coming soon....</h3></p>
+      <header>
+        <h1>Atithi Devi Bhav</h1>
+      </header>     
+      {monsters && monsters.length && <input onChange={(event) => setSearchKey(event.target.value)}></input>}
+      {isLoading && <h3>Loading...</h3>}
+      {monsters && monsters.length && monsters.filter(item => item?.name?.toLowerCase().includes(searchKey.toLowerCase())).map((monster, index) => {
+        return <h3 key={index}>{monster?.name}</h3>;
+      })}
     </div>
   );
 }
