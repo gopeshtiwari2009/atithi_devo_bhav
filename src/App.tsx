22e94888defcecfefbb50a1faae651264c4a0610
Gopesh <gopeshtiwari2009@gmail.com>
2022-07-31 20:24:45 +0530

    CSS grid implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { Grid, makeStyles, Paper } from '@mui/material';
 import React, { ChangeEvent, useEffect, useState } from 'react';
 import './App.css';
 
@@ -18,11 +17,11 @@ function GridItem({ monster }: GridItemProps) {
     // From 0 to 600px wide (smart-phones), I take up 12 columns, or the whole device width!
     // From 600-690px wide (tablets), I take up 6 out of 12 columns, so 2 columns fit the screen.
     // From 960px wide and above, I take up 25% of the device (3/12), so 4 columns fit the screen.
-    <Grid className="grid-item" item xs={6} sm={3} md={3}>
+    <div className="grid-item">
           <img className='image' src='https://www.w3schools.com/howto/img_avatar.png'></img>
           <h3>{monster.name}</h3>
           <div>{monster.email}</div>
-    </Grid>
+    </div>
   );
 }
 
@@ -32,6 +31,7 @@ function App() {
   const initialMonsters: Monster[] = [];
   const [monsters, setMonsters] = useState(initialMonsters);
   const [searchKey, setSearchKey] = useState('');
+  const [isDarkMode, setIsDarkMode] = useState(false);
   let filteredArray = monsters;
   filteredArray = monsters.filter((monster => monster.name.toLocaleLowerCase().includes(searchKey)));
 
@@ -51,13 +51,15 @@ function App() {
       <header>
         <h1>Atithi Devi Bhav</h1>
       </header>
+      <button onClick={() => setIsDarkMode(!isDarkMode)}>Change Theme</button>
       {filteredArray && filteredArray.length && <input type="search" placeholder="search monsters" onChange={onSearchKeyChanged}></input>}
+      {filteredArray && !filteredArray.length && !isLoading && <p> There are no monsters</p> }
       {isLoading && <h3>Loading...</h3>}
-      <Grid container spacing={0} className="grid-container">
+      <div className={isDarkMode ? 'grid-container-dark' : 'grid-container-default'}>
       {filteredArray && filteredArray.length && filteredArray.map((monster, index) => {
         return <GridItem key={index} monster={monster} />
       })}
-      </Grid>
+      </div>
     </div>
   );
 }
