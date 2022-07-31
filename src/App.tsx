6f57bf3d4246124ea0829ac42698100d2c288d17
Gopesh <gopeshtiwari2009@gmail.com>
2022-07-31 11:33:35 +0530

    styling c hanged

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ function GridItem({ monster }: GridItemProps) {
     // From 0 to 600px wide (smart-phones), I take up 12 columns, or the whole device width!
     // From 600-690px wide (tablets), I take up 6 out of 12 columns, so 2 columns fit the screen.
     // From 960px wide and above, I take up 25% of the device (3/12), so 4 columns fit the screen.
-    <Grid item xs={6} sm={3} md={3}>
+    <Grid className="grid-item" item xs={6} sm={3} md={3}>
           <img className='image' src='https://www.w3schools.com/howto/img_avatar.png'></img>
           <h3>{monster.name}</h3>
           <div>{monster.email}</div>
@@ -53,7 +53,7 @@ function App() {
       </header>
       {filteredArray && filteredArray.length && <input type="search" placeholder="search monsters" onChange={onSearchKeyChanged}></input>}
       {isLoading && <h3>Loading...</h3>}
-      <Grid container spacing={0}>
+      <Grid container spacing={0} className="grid-container">
       {filteredArray && filteredArray.length && filteredArray.map((monster, index) => {
         return <GridItem key={index} monster={monster} />
       })}
